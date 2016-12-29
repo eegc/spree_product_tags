dd6939aa9e9ac3810ac00f849bf95053ffb38276
Enrique <engarcia@acid.cl>
2016-12-29 18:19:06 -0300

    Add migrations

---
+++ db/migrate/20161229194048_change_collation_for_spree_tag_names.rb
@@ -0,0 +1,7 @@
+class ChangeCollationForSpreeTagNames < ActiveRecord::Migration
+  def up
+    if ActsAsTaggableOn::Utils.using_mysql?
+      execute("ALTER TABLE spree_tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
+    end
+  end
+end
